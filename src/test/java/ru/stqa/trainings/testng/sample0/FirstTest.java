f39fa8e718f754ecf5236733f5ca15caaa191069
Alexei Barantsev <barancev@gmail.com>
2014-05-12 15:44:55 +0400

    Step 1. Maven + TestNG

---
+++ src/test/java/ru/stqa/trainings/testng/sample0/FirstTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.trainings.testng.sample0;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+  @Test
+  public void test1() {
+    System.out.println("FirstTest.test1");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("FirstTest.test2");
+  }
+}
---
+++ src/test/java/ru/stqa/trainings/testng/sample0/SecondTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.trainings.testng.sample0;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+  @Test
+  public void test1() {
+    System.out.println("SecondTest.test1");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("SecondTest.test2");
+  }
+}
